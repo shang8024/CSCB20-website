519364c8876523c25a39cf101934a318d6fe1ad0
shang8024 <58407413+shang8024@users.noreply.github.com>
2021-03-28 07:02:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ def close_connection(exception):
         db.close()
     
 @app.route('/')
-def root():
-    if not session.get('logged_in'):
+def home():
+    if not 'user' in session:
         return render_template('index.html',)
     else:
         return render_template('home.html',user=session['user'])
@@ -65,7 +65,6 @@ def login():
         if user == None:
             error = 'Invalid username'
         elif login_pass == user['password']:
-            session['logged_in'] = True
             session['user'] = {
                 'username': user['username'],
                 'name': [user['first_name'],user['last_name']],
@@ -78,7 +77,10 @@ def login():
         return redirect(url_for('home'))
     return render_template('index.html',error=error)
 
-
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect(url_for('home'))
 @app.route('/signup',methods=['GET', 'POST'])
 def signup():
     db = get_db()
@@ -124,7 +126,7 @@ def request_remark():
     db.row_factory = make_dicts
     remark_req = request.form['remark_request']
     remark_eve = request.form['remark_event']
-    query = "update Grades set remark=1, request='%s' where username='%s' and eid in (select eid from Events where ename='%s')" % (remark_req,session['user']['username'],remark_eve)
+    query = "update Grades set remark=1, request='%s' where username='%s' and ename='%s'" % (remark_req,session['user']['username'],remark_eve)
     query_db(query)
     db.commit()
     db.close() 
@@ -140,10 +142,28 @@ def grading():
     db=get_db()
     db.row_factory = make_dicts
     if (not grade) or (not student) or (not event):
+        # some of grade,student or event type inputs are None, return with error
         error = "None of grade,username and type can be empty."
         return render_template('grade.html',user=session['user'],grade=get_grade_table(),error=error)
     if not query_db("select * from Takes where username='%s' and cid in (select cid from Takes where username='%s')" % (student,user)):
+        # student does not exist or is not in the instructors' class.
         error = "The student is not in your class."
+        return render_template('grade.html',user=session['user'],grade=get_grade_table(),error=error)
+    grades=query_db("select * from Grades where username='%s' and ename='%s'" % (student,event),one=True)
+    if not grades:
+        # grade(student,event) does not exist
+        # insert event to Events(ename) if not exist
+        query_db("insert into Events(ename) select '%s' where not exists(select 1 from Events where ename='%s')" % (event,event))
+        db.commit()
+        # insert value(student,event,grade,0) into Events(username,ename,grade,remark)
+        query_db("insert into Grades(username,ename,grade,remark) values('%s','%s',%s,0)" % (student,event,grade)) 
+    else:
+        # grade(student,event) exist, update existing row.
+        if grades['remark'] == 1:
+            # if the student is requesting a remark for that grade, set the remark status to 'remarked'
+            grades['remark'] = -1
+        query_db("update Grades set remark=%d, grade=%s where ename='%s' and username='%s'" % (grades['remark'],grade,event,student))
+    db.commit()
     return render_template('grade.html',user=session['user'],grade=get_grade_table(),error=error)
 
 @app.route("/remark-sort")
